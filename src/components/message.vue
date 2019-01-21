0bda0066876923f7381e91426857efa006d03c99
Jurek Weber <jurek@afrika2.de>
2019-01-21 20:01:23 +0100

    messages

---
+++ src/components/message.vue
@@ -0,0 +1,35 @@
+<template>
+    <div :class="type">
+        <span :class="type">
+        <slot></slot>
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'message',
+    props: {
+        type: {
+            validator: function (value) {
+                return ['error'].indexOf(value) !== -1;
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+div.error {
+    background-color: #fce4e4;
+    border: 1px solid #fcc2c3;
+    float: left;
+    padding: 10px 10px;
+    margin-bottom: 10px;
+}
+
+span.error {
+    color: #cc0033;
+    font-size: 13px;
+}
+</style>
