0cfc768e98c5be1fd6932a52280055fa48615770
Jurek Weber <jurek@afrika2.de>
2019-01-21 20:01:05 +0100

    adding nice login

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,91 +1,113 @@
 <template>
-    <div class="login-container">
-        <form class="pure-form" id="login-wrapper">
-            <div id="frame">
-                <img src="http://plg-berlin.de/images/LOGOBT.png" width="180px" style="margin-top: 35px;" draggable="false">
-                <div class="ti" id="username">
-                    <i class="fas fa-user"></i>
-                    <input type="text" placeholder="Benutzername">
-                </div>
-                <div class="ti" id="password">
-                    <i class="fas fa-lock"></i>
-                    <input type="password" placeholder="Passwort">
-                </div>
-                <button id="lbtn" class="pure-button" @click="login()">Login <i class="fas fa-sign-in-alt"></i></button>
-            </div>
-        </form>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    </div>
+	<div class="login-background">
+		<form class="login-form" v-on:submit.prevent="login()">
+			<h1 class="login-title">Bitte melden sie sich an:</h1>
+			<message type="error" v-show="error" style="width:285px;">
+				<center>Fehler bei der Anmeldung.</center>
+			</message>
+			<input type="text" v-model="mail" class="login-input" placeholder="E-Mail" autofocus>
+			<input type="password" v-model="password" class="login-input" placeholder="Passwort">
+			<input type="submit" value="Anmelden" class="login-button">
+			<p class="forgot-password"><a @click="forgotLogin()">Passwort vergessen?</a></p>
+		</form>
+	</div>
 </template>
 
 <script>
 import store from '../store'
-import login from '../services/login'
+//import login from '../services/login'
+import message from './message'
+
 
 export default {
-    name: 'login',
-    store: store,
-    data: function () {
-        return {}
-    },
-    methods: {
-        login: function () {
-
-            this.$store.commit("toggle_login")
-        }
-    }
+	name: 'login',
+	store: store,
+	components: {message},
+	data: function () {
+		return {
+			error: false,
+			password: null,
+			mail: null
+		}
+	},
+	methods: {
+		login: function () {
+			this.error = true;
+			/*login.check(this.mail, this.mail)
+			.then(function (response) {
+				console.log(response)
+			})
+			.catch(function (error) {
+				console.log(error);
+			})*/
+			this.password = null;
+			this.$store.commit("toggle_login")
+		},
+		forgotLogin: function () {
+			alert("Pech gehabt")
+		}
+	}
 }
 </script>
 
-<style scoped>
-.fa-lock, .fa-user {
-    color: white;
-}
-.login-container {
-    position: absolute;
-    background: #28356c;
-    height: 100%;
-    width: 100%;
-}
-
-#login-wrapper {
-    position: absolute;
-    top: 25%;
-    min-width: 100% !important;
-    text-align: center;
-    align-content: center;
-    align-items: center;
-    align-self: center;
+<style>
+.login-form {
+	margin: 20px auto;
+	width: 340px;
+	padding: 30px 25px;
+	background: white;
+	border: 1px solid #c4c4c4;
+	border-radius: 20px;
+	overflow:hidden;
 }
 
-#frame {
-    position: absolute;
-    left: 50%;
-    transform: translate(-50%);
-    width: 290;
-    border-style: solid;
-    border-color: #ffffff;
-    border-width: 2px;
+h1.login-title {
+	margin: -30px -25px 25px;
+	padding: 15px 25px;
+	line-height: 30px;
+	font-size: 25px;
+	font-weight: 300;
+	color: grey;
+	text-align:center;
+	background: #f7f7f7; 
 }
 
-#username {
-    margin-top: 35px;
-}
-
-#password {
-    margin-bottom: 10px;
-}
-
-#lbtn {
-	background: transparent;
+.login-input {
+	width: 285px;
+	height: 50px;
 	margin-bottom: 25px;
-	border-style: solid;
-	border-color: #fff;
-	border-width: 2px;
-	color: white;
+	padding-left:10px;
+	font-size: 15px;
+	background: #fff;
+	border: 1px solid #ccc;
+	border-radius: 4px;
+}
+.login-input:focus {
+	border-color: #888;
+	outline: none;
+}
+.login-button {
+	width: 285px;
+	height: 50px;
+	padding: 0;
+	font-size: 20px;
+	color: #fff;
+	text-align: center;
+	background: #f25e41;
+	border: 0;
+	border-radius: 5px;
+	cursor: pointer; 
+	outline:0;
 }
 
-.ti {
-    padding: 6px;
+.forgot-password {
+	text-align:center;
+	margin-bottom:0px;
+}
+.forgot-password a {
+	color:#666;
+	text-decoration:none;
+	font-size:13px;
+	cursor: pointer;
 }
 </style>
